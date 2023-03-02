a42ab68bbccd7e9e410b22a02f6b49c910f3ecc2
alksndrkshn <alksndrkshn@gmail.com>
2023-03-02 22:14:47 +0400

    Общая фикстура для всех тестов

--- fixture/group.py
+++ fixture/group.py
@@ -28,6 +28,7 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+
     def delete_first_group(self):
         wd = self.app.wd
         self.open_group_page()
@@ -36,6 +37,7 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+
 
     def return_to_groups_page(self):
         wd = self.app.wd
