3f047aadd82e0e00409c64178e8b4617f0a006d5
alksndrkshn <alksndrkshn@gmail.com>
2023-03-16 23:12:18 +0400

    Оптимизация ввода данных формы

--- test/test_edit_first_group.py
+++ test/test_edit_first_group.py
@@ -1,3 +1,6 @@
 from model.group import Group
-def test_edit_first_group(app):
-    app.group.edit_first_group(Group(name="edit", header="4321", footer="4321"))
+def test_edit_first_group_name(app):
+    app.group.edit_first_group(Group(name="edit name group"))
+
+def test_edit_first_group_header(app):
+    app.group.edit_first_group(Group(header="edit header group"))
