cf39bcb0ffa28fbd7ca8963b827f2b6949c96d65
alksndrkshn <alksndrkshn@gmail.com>
2023-03-16 22:06:28 +0400

    Валидация фикстуры перед каждым тестом Убрал логин логаут из всех тестов

--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -5,17 +5,13 @@ from model.contact import Contact
 
 def test_create_contact(app):
         app.open_home_page()
-        app.session.login(username="admin", password="secret")
         app.contact.init_create_new_contact()
         app.contact.fill_form(Contact(firstname="Alex", middlename="Smith", nickname="AlSmit"))
         app.contact.submit_create_new_contact()
-        app.session.logout()
 
 def test_create_empty_contact(app):
         app.open_home_page()
-        app.session.login(username="admin", password="secret")
         app.contact.init_create_new_contact()
         app.contact.fill_form(Contact(firstname="", middlename="", nickname=""))
         app.contact.submit_create_new_contact()
-        app.session.logout()
 
--- test/test_edit_first_group.py
+++ test/test_edit_first_group.py
@@ -1,5 +1,3 @@
 from model.group import Group
 def test_edit_first_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name="edit", header="4321", footer="4321"))
-    app.session.logout()
