6f33787421f29bbe0621502cf7bd072e3cef715c
alksndrkshn <alksndrkshn@gmail.com>
2023-03-19 18:46:56 +0400

    Предусловие на модификацию контакта

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,5 +1,6 @@
 from model.contact import Contact
 def test_edit_first_contact(app):
-    app.open_home_page()
+    if app.contact.is_list_empty() == 0:
+        app.contact.create_new(Contact(firstname="empty contact", middlename="empty contact", nickname="empty contact"))
     app.contact.edit_first_contact(Contact(firstname="Aleksnadr", middlename="Sergeevich", nickname="AlSmit1"))
     app.contact.submit_edit_contact()
