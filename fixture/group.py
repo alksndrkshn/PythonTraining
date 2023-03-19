076a222693d7ccf2d346bea1d9a002062a085050
alksndrkshn <alksndrkshn@gmail.com>
2023-03-19 16:50:42 +0400

    Предусловие на удаление и модификацию групп

--- fixture/group.py
+++ fixture/group.py
@@ -63,4 +63,4 @@ class GroupHelper:
     def count(self):
         wd = self.app.wd
         self.open_group_page()
-        return len(wd.find_element_by_name("selected[]"))
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_edit_first_group.py
+++ test/test_edit_first_group.py
@@ -1,6 +1,10 @@
 from model.group import Group
 def test_edit_first_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="empty_name"))
     app.group.edit_first_group(Group(name="edit name group"))
 
 def test_edit_first_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="empty_header"))
     app.group.edit_first_group(Group(header="edit header group"))
