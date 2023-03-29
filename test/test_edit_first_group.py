cf9c589bc3e5053d390a25eb7a57dd2c5e262c9a
alksndrkshn <alksndrkshn@gmail.com>
2023-03-29 14:37:56 +0400

    Сортировка списков перед сравнением

--- test/test_edit_first_group.py
+++ test/test_edit_first_group.py
@@ -3,9 +3,13 @@ def test_edit_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="empty_name"))
     old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(name="edit name group"))
+    group = Group(name="edit name group")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_edit_first_group_header(app):
     if app.group.count() == 0:
