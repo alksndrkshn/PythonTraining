cc44c0da11469a9b9f0a5e119ab0b9411bcf1a75
alksndrkshn <alksndrkshn@gmail.com>
2023-04-11 13:41:36 +0400

    Исправил ошибку в assert

--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -3,7 +3,7 @@ import pytest
 from model.contact import Contact
 def test_create_contact(app):
         old_contacts = app.contact.get_contact_list()
-        contact = Contact(firstname="name_test", middlename="middle_test", lastname="last_test", nickname="nick_test")
+        contact = Contact(firstname="name_test", middlename="middle_test"Bc, nickname="nick_test")
         app.contact.create_new(contact)
         assert len(old_contacts) + 1 == app.contact.count()
         new_contacts = app.contact.get_contact_list()
