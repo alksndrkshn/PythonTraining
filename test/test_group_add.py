09f748700e704fbc64f2703e3b834fe2ed21cc10
alksndrkshn <alksndrkshn@gmail.com>
2023-04-14 10:13:35 +0400

    Отделение тестовых данных от сценариев

--- test/test_group_add.py
+++ test/test_group_add.py
@@ -1,18 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 from model.group import Group
-import random
-import string
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(5)
-]
+from data.add_group import constant as testdata
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
@@ -22,7 +11,3 @@ def test_add_group(app, group):
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-
-
