817659b35ab96bbbb1b1224d13b25e950de45fad
alksndrkshn <alksndrkshn@gmail.com>
2023-04-14 18:05:17 +0400

    Переделаны тесты групп, чтобы сравнивались данные, загруженные из БД

--- fixture/group.py
+++ fixture/group.py
@@ -106,3 +106,12 @@ class GroupHelper:
                 self.group_cache.append(Group(name=text, id=id))
         return list(self.group_cache)
 
+    def edit_group_by_id(self, id, new_group_data):
+        wd = self.app.wd
+        self.open_group_page()
+        self.select_group_by_id(id)
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
--- test/test_edit_first_group.py
+++ test/test_edit_first_group.py
@@ -1,22 +1,21 @@
 from model.group import Group
 from random import randrange
-def test_edit_first_group_name(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="empty_name"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    group = Group(name="edit_name_group")
-    group.id = old_groups[index].id
-    app.group.edit_group_by_index(index, group)
-    assert len(old_groups) == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index] = group
+def test_edit_first_group(app, db, check_ui):
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name="name", header="head", footer="foot"))
+    old_groups = db.get_group_list()
+    group = Group(name="edited_group_name", header="edited_header", footer="edited_footer")
+    random_group = randrange(len(old_groups))
+    group.id = old_groups[random_group].id
+    app.group.edit_group_by_id(group.id, group)
+    new_groups = db.get_group_list()
+    old_groups[random_group] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_edit_first_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="empty_header"))
-    old_groups = app.group.get_group_list()
-    app.group.edit_first_group(Group(header="edit_header_group"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+#def test_edit_first_group_header(app):
+#    if app.group.count() == 0:
+#        app.group.create(Group(name="empty_header"))
+#    old_groups = app.group.get_group_list()
+#    app.group.edit_first_group(Group(header="edit_header_group"))
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups) == len(new_groups)
