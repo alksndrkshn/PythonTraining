14e0d43c7b612a62c8154c22d507aba018b5063e
alksndrkshn <alksndrkshn@gmail.com>
2023-04-14 11:32:06 +0400

    Загрузка тестовых данных из файлов в формате JSON

--- generator/group.py
+++ generator/group.py
@@ -1,4 +1,4 @@
-import json
+import jsonpickle
 from model.group import Group
 import random
 import string
@@ -21,6 +21,7 @@ for o, a in opts:
         n = int(a)
     elif o == "-f":
         f = a
+
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
@@ -34,4 +35,5 @@ testdata = [Group(name="", header="", footer="")] + [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
