690a0043437dc92cd444728a8492e886a0e492ac
alksndrkshn <alksndrkshn@gmail.com>
2023-04-19 11:46:03 +0400

    Правки по 21 заданию

--- test/test_phones.py
+++ test/test_phones.py
@@ -50,7 +50,7 @@ def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
-                                       [contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone]))))
+                                       [contact.homephone, contact.mobilephone, contact.workphone]))))
 
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
